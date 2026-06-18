2e853bf27bad03492c5cfc878cfae5682aff7960
sya-cloud <m-9198667@moe-dl.edu.my>
2026-06-18 08:56:36 +0800

    Update hello_ai.py

--- hello_ai.py
+++ hello_ai.py
@@ -1,1 +1,3 @@
-print("Welcome to Computer Vision Playground!)
+print("Welcome to Computer Vision Playground!")
+print("This repository is used for learning GitHub workflow.")
+print("AI and Computer Vision are exciting technologies!")
